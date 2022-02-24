c1cf1bf1719a90a4a60e91bcca2c1e7379a3b4bd
Lucas Araujo Oliveira Silva <37140363854@info.scs>
2022-02-24 08:34:51 -0300

    Inicia o desenvolvimento da interface

---
+++ classificados-base/js/script.js
@@ -0,0 +1,43 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log('teste');
+  };
+  
+  let cartItem = document.querySelector('.cart-items-container');
+  
+  document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    console.log('teste');
+  };
+
+  let searchForm = document.querySelector('.search-form');
+  
+  document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log('teste');
+  };
+
+  let userForm = document.querySelector('.user-form');
+
+  document.querySelector('#user-btn').onclick = () =>{
+    userForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log('teste');
+  }
+
+  window.onscroll = () =>{
+      navbar.classList.remove('active');
+      searchForm.classList.remove('active');
+      cartItem.classList.remove('active');
+  }
+
