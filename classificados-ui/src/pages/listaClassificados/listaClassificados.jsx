437756c71f08c33024a8d0ad77a643b23fce6cae
Lucas Araujo Oliveira Silva <37140363854@info.scs>
2022-03-03 10:18:44 -0300

    Tentando implementar filtro de pesquisa

--- classificados-ui/src/pages/listaClassificados/listaClassificados.jsx
+++ classificados-ui/src/pages/listaClassificados/listaClassificados.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../../styles/listaClassificados.css"
 import "../../styles/global.css"
 import "https://code.iconify.design/2/2.1.2/iconify.min.js"
@@ -14,6 +14,7 @@ export default function ListaClassificados() {
     const [animationState, setAnimationState] = useState({
         isStopped: true, isPaused: false
     });
+    const [filtroCategorias, setFiltroCategorias] = useState([])
 
     const defaultOptions = {
         loop: false,
@@ -23,6 +24,26 @@ export default function ListaClassificados() {
             preserveAspectRatio: 'xMidYMid slice'
         }
     };
+
+    function FiltroCategoria(check) {
+
+        if (check.target.checked === true) {
+            setFiltroCategorias(filtroCategorias => [...filtroCategorias, check.target.value])
+            console.log(filtroCategorias)
+        }
+
+        ListarClassificados()
+        // else {
+        //     setFiltroCategorias(filtroCategorias => (filtroCategorias.splice(filtroCategorias.indexOf(check.target.value), 1)))
+        //     console.log(filtroCategorias)
+        // }
+
+
+
+        // let listaCategorias = ['']
+        // listaCategorias.push(categoria)
+        // setFiltroCategorias(listaCategorias)
+    }
 
     function ListarClassificados() {
         // api.get('/classificados', {
@@ -38,33 +59,37 @@ export default function ListaClassificados() {
                 }
             })
             .catch((erro) => console.log(erro))
+
+        // if (filtroCategorias !== null) {
+        //     let teste = listaClassificados.map((classificado) => {
+        //         classificado === filtroCategorias.map((categoria) => { return categoria })
+        //     })
+
+        //     console.log(teste)
+        // }
+
+        
     }
 
-    function animacaoLottie(idClassificado) {
-
-        // Animated.timing(idClassificado.progress, {
-        //     toValue: 1,
-        //     duration: 5000,
-        //     easing: Easing.linear,
-        // }).start();
+    function FazerReserva(idPedidoEscolhido) {
 
         setAnimationState({
             ...animationState,
             isStopped: !animationState.isStopped
         })
-    }
 
-    function FazerReserva(idPedidoEscolhido) {
         let reserva = {
-            idUsuario: parseJwt().jti,
+            idUsuario: 2,
             idPedido: idPedidoEscolhido
         }
 
-        api.post("/reservas", reserva, {
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('xxxxxxxxxxxxxxx'),
-            }
-        })
+        api.post("/reserva", reserva
+            // {
+            //     headers: {
+            //         Authorization: 'Bearer ' + localStorage.getItem('xxxxxxxxxxxxxxx'),
+            //     }
+            // }
+        )
             .catch((erro) => {
                 console.log(erro)
             })
@@ -96,11 +121,10 @@ export default function ListaClassificados() {
                                                     <img className="listaDemandas__imgPerfil" src={classificado.Imagem}
                                                         alt="" />
                                                     <h2>{classificado.Titulo}</h2>
-                                                    <button className="animation" onClick={() => animacaoLottie(classificado.id)}>
+                                                    <button className="animation" onClick={() => FazerReserva(classificado.id)}>
                                                         <Lottie options={defaultOptions}
                                                             height={60}
                                                             width={60}
-                                                            progress={classificado.id.progress}
                                                             isStopped={animationState.isStopped}
                                                             isPaused={animationState.isPaused} />
                                                     </button>
@@ -133,18 +157,18 @@ export default function ListaClassificados() {
                                 <div className="listaDemandas__categorias">
                                     <h3>Categorias</h3>
                                     <ul>
-                                        <li><input type="checkbox" />Beleza</li>
-                                        <li><input type="checkbox" />Cultura</li>
-                                        <li><input type="checkbox" />Eletrônicos</li>
-                                        <li><input type="checkbox" />Esportes</li>
-                                        <li><input type="checkbox" />Comida</li>
-                                        <li><input type="checkbox" />Mercados</li>
-                                        <li><input type="checkbox" />Pets</li>
-                                        <li><input type="checkbox" />Saúde</li>
-                                        <li><input type="checkbox" />Serviços</li>
-                                        <li><input type="checkbox" />Turismo</li>
-                                        <li><input type="checkbox" />Veículos</li>
-                                        <li><input type="checkbox" />Empregos</li>
+                                        <li><input value="Beleza" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Beleza</li>
+                                        <li><input value="Cultura" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Cultura</li>
+                                        <li><input value="Eletrônicos" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Eletrônicos</li>
+                                        <li><input value="Esportes" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Esportes</li>
+                                        <li><input value="Comida" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Comida</li>
+                                        <li><input value="Mercados" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Mercados</li>
+                                        <li><input value="Pets" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Pets</li>
+                                        <li><input value="Saúde" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Saúde</li>
+                                        <li><input value="Serviços" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Serviços</li>
+                                        <li><input value="Turismo" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Turismo</li>
+                                        <li><input value="Veículos" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Veículos</li>
+                                        <li><input value="Empregos" onClick={(x) => FiltroCategoria(x)} type="checkbox" />Empregos</li>
                                     </ul>
                                 </div>
                                 <div className="listaDemandas__regiao">
